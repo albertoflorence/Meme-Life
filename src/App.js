25fa63d74bd8b5eb19fc19556f944ea4299a93c0
albertoflorence <alberto.sabatier@gmail.com>
2018-01-20 20:41:56 -0200

    material-ui added and initialized with theme

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles'
+import { blue, red } from 'material-ui/colors'
+import Post from './components/Post'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: blue[300],
+      main: blue[500],
+      dark: blue[700]
+    },
+    secondary: {
+      light: red[300],
+      main: red[500],
+      dark: red[700]
+    }
+  }
+})
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <MuiThemeProvider theme={theme}>
+        <div>
+          <div style={{ height: '80px' }} />
+          <Post />
+        </div>
+      </MuiThemeProvider>
     )
   }
 }
 
-export default App;
+export default App
