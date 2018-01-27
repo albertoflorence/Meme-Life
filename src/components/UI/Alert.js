49a524bf5eb417354b85e7bf5b2197ae8e292ee7
albertoflorence <alberto.sabatier@gmail.com>
2018-01-27 02:40:36 -0200

    alert component

---
+++ src/components/UI/Alert.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import withStyles from 'material-ui/styles/withStyles'
+
+const style = theme => ({
+  wrapper: {
+    padding: '20px',
+    borderRadius: '10px'
+  },
+  success: {
+    backgroundColor: '#c8e6c9 ',
+    color: '#2e7d32'
+  },
+  danger: {
+    backgroundColor: '#ffcdd2',
+    color: '#c62828'
+  },
+  info: {
+    backgroundColor: '#bbdefb',
+    color: '#1565c0'
+  },
+  warning: {
+    backgroundColor: '#ffe0b2',
+    color: '#ef6c00'
+  }
+})
+
+const Alert = ({ type, children, classes, ...props }) => {
+  const className = [classes[type], classes.wrapper]
+  if (!children) return null
+  return (
+    <div {...props} className={className.join(' ')}>
+      {children}
+    </div>
+  )
+}
+
+export default withStyles(style)(Alert)
